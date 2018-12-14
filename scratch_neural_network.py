c6a5f519cdefd52c386fbfa69fb9333a979b139e
Sukumar Gaonkar <sukumargaonkar@gmail.com>
2018-12-14 13:24:02 -0500

    single epoch code done

--- scratch_neural_network.py
+++ scratch_neural_network.py
@@ -5,12 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-learning_rate = 0.1
-filename = "ip1_min.jpg"
+learning_rate = 0.01
+filename = "ip2_min.jpg"
 INPUT_IMAGE_SRC = "C:/Users/sukum/OneDrive/Academic/Grad_Rutgers/Intro To AI (520)/Assignments/Assignment_4/image_colorizer/" + filename
 weight_mat = []
-weight_mat.append([[rnd.uniform(0, 1) for x in range(6)] for y in range(9+1)])
-weight_mat.append([[rnd.uniform(0, 1) for x in range(3)] for y in range(6+1)])
+output_image = None
+counter = 0
+weight_mat.append([[rnd.uniform(0, 0.5) for x in range(6)] for y in range(9+1)])
+weight_mat.append([[rnd.uniform(0, 0.5) for x in range(3)] for y in range(6+1)])
 
 error_list = []
 
@@ -29,13 +31,13 @@ def generate_grid(i, j, img):
 def predict(input_values):
     r, g, b, = 10, 10, 10
     # curr_inputs = np.matrix(input_values + [1], dtype=float)
-    curr_inputs = input_values
+    curr_inputs = [x/255 for x in input_values]
     for i in range(len(weight_mat)):
         # curr_weights = np.matrix(weight_mat[i], dtype=float)
         # curr_inputs = np.matmul(curr_inputs, curr_weights)
         curr_inputs = np.matmul(curr_inputs + [1], weight_mat[i])
-        max_val = max(abs(curr_inputs))
-        curr_inputs = list(map(lambda x: x/max_val, curr_inputs))
+        # max_val = max(abs(curr_inputs))
+        # curr_inputs = list(map(lambda x: x/max_val, curr_inputs))
         curr_inputs = list(map(lambda x: sigmoid(x), curr_inputs))
 
     return curr_inputs
@@ -43,8 +45,24 @@ def predict(input_values):
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
 
+def colorize_img(img_gray):
+    global counter
+
+    ip_gray_height, ip_gray_width = img_gray.shape
+
+    for row in range(ip_gray_height):
+        for col in range(ip_gray_width):
+            output_image[row][col] = tuple(map(lambda x: x * 255, predict(generate_grid(row, col, ip_gray))))
+
+    counter = counter + 1
+    cv2.imwrite("results/op_img_" + str(counter) + ".jpg", output_image)
+
+
 if __name__ == "__main__":
+    f = open("log.txt", "w")
+
     ip_img = cv2.imread(INPUT_IMAGE_SRC)
+    output_image = np.zeros((ip_img.shape[0], ip_img.shape[1], 3), np.float64)
     # cv2.imshow("Input", ip_img)
     ip_blue, ip_green, ip_red = cv2.split(ip_img)
 
@@ -55,7 +73,10 @@ if __name__ == "__main__":
     ip_gray_height, ip_gray_width = ip_gray.shape
     loss = None
 
-    # Traverse each pixel
+    colorize_img(ip_gray)
+
+    # Epoch
+
     for row in range(ip_gray_height):
         for col in range(ip_gray_width):
             # for channel in range(3):
@@ -81,24 +102,24 @@ if __name__ == "__main__":
                     for y in range(len(weight_mat[i][0])):
                         weight_mat[i][x][y] = weight_mat[i][x][y] + learning_rate * error_list[y] * (weight_mat[i][x][y] / weight_sum[i][y])
                         new_error_list[x] = new_error_list[x] + error_list[y] * (weight_mat[i][x][y] / weight_sum[i][y])
+                        error_list = new_error_list
 
                 if i == len(weight_mat) - 1:
                     epoc_loss = error_list
 
-                error_list = new_error_list
 
 
-                print(weight_mat[0])
-                print(row/ip_gray_height * 100, "%")
+            f.write("Loss" + str(["{:.2f}".format(value) for value in epoc_loss]) + "\n")
+
+            f.write(str([["{:.2f}".format(y) for y in x] for x in weight_mat[0]]) + "\n")
+            print(row/ip_gray_height * 100, "%")
+
+        if row%10 == 0:
+            colorize_img(ip_gray)
 
     # Test the Neural Network
-    output_image = np.zeros((ip_img.shape[0], ip_img.shape[1], 3), np.float64)
 
-    for row in range(ip_gray_height):
-        for col in range(ip_gray_width):
-            output_image[row][col] = tuple(map(lambda x: x*255, predict(generate_grid(row, col, ip_gray))))
 
-    cv2.imwrite('op_img.jpg', output_image)
     print("done")
 
             # for i in range(len(weight_mat) - 1, -1, -1):
